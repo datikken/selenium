cd8a380b780b039d01d6758e8947aa75dbf8a5b5
nshadybekov <nshadybekov@adsterra.com>
2022-04-30 17:43:19 +0300

    get all links on page

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.firefox_binary = r'/usr/local/bin/geckodriver'
 driver = webdriver.Firefox(options=options)
-driver.get("http://lacoste.ru")
+# driver.get("http://lacoste.ru")
+
+driver.get('https://kiwitaxi.com')
+links = driver.find_elements(By.TAG_NAME, "a")
+
+with open('links.txt', 'w') as f:
+    for link in links:
+        f.write(link.get_attribute('href'))
+        f.write('\n')
+
 # assert "Python" in driver.title
 
 # elem = driver.find_element(By.NAME, "q")
@@ -15,4 +24,4 @@ driver.get("http://lacoste.ru")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
 
-# driver.close()
+driver.close()
